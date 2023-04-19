0e99ed0a958163dc948507a78a7f27b33b203ef9
Rohit kumar <Rohit.kumar09@nagarro.com>
2023-04-19 17:51:27 +0530

    Initial commit

---
+++ socket/index.js
@@ -0,0 +1,55 @@
+import { Server } from "socket.io";
+
+const io = new Server({
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+// let onlineUsers = [];
+
+// const addNewUser = (username, socketId) => {
+// !onlineUsers.some((user) => user.username === username) &&
+//     onlineUsers.push({ username, socketId });
+// };
+
+// const removeUser = (socketId) => {
+//   onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+// };
+
+// const getUser = (username) => {
+//   console.log("onlineusers", onlineUsers);
+//   return onlineUsers.find((user) => user.username === username);
+// };
+
+io.on("connection", (socket) => {
+  // socket.on("newUser", (username) => {
+  //   console.log(username);
+  //   addNewUser(username, socket.id);
+  // });
+
+  // socket.on("sendNotification", ({ senderName, receiverName, type }) => {
+  //   console.log("sendNotification", senderName, receiverName);
+  //   const receiver = getUser(receiverName);
+  //   io.to(receiver.socketId).emit("getNotification", {
+  //     senderName,
+  //     type,
+  //   });
+  // });
+
+  io.emit("sample", { abc: "some test data from localhost:5000123kkkkm" });
+
+  // socket.on("sendText", ({ senderName, receiverName, text }) => {
+  //   const receiver = getUser(receiverName);
+  //   io.to(receiver.socketId).emit("getText", {
+  //     senderName,
+  //     text,
+  //   });
+  // });
+
+  // socket.on("disconnect", () => {
+  //   removeUser(socket.id);
+  // });
+});
+
+io.listen(5000);
